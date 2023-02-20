11e667c6d659ea4e7a7510bda3a7778a0cfe60b6
Regis Alenda <regis.alenda@finbourne.com>
2023-02-20 22:30:55 +0000

    Add base repository test and use it to test the in memory implementation

---
+++ MarketDataGateway/MarketDataGateway.Infrastructure.Tests/InMemoryMarketDataContributionStoreTests.cs
@@ -0,0 +1,12 @@
+﻿using MarketDataGateway.Infrastructure.MarketDataContributionRepository;
+using MarketDataGateway.Model;
+using MarketDataGateway.Model.Tests;
+using NUnit.Framework;
+
+namespace MarketDataGateway.Infrastructure.Tests;
+
+[TestFixture]
+public class InMemoryMarketDataContributionStoreTests : MarketDataContributionRepositoryTestBase
+{
+    protected override IMarketDataContributionRepository GetRepository() => new InMemoryMarketDataContributionRepository();
+}
---
+++ MarketDataGateway/MarketDataGateway.Model.Tests/MarketDataContributionRepositoryTestBase.cs
@@ -0,0 +1,30 @@
+﻿using MarketDataGateway.Model.FxQuote;
+using MarketDataGateway.Model.MarketDataValidation;
+using NUnit.Framework;
+
+namespace MarketDataGateway.Model.Tests;
+
+/// <summary>
+/// Base test class to test generic repository logic - we can then instanciate that with different repository implementation
+/// </summary>
+[TestFixture]
+public abstract class MarketDataContributionRepositoryTestBase
+{
+    protected abstract IMarketDataContributionRepository GetRepository();
+
+    [Test]
+    public async Task Stores_Multiple_Identical_FxQuotes_Throws()
+    {
+        var timestamp = DateTimeOffset.UtcNow;
+        CurrencyPair.From("Eur/USD", out var pair, out var _);
+        var request1 = MarketDataContributionRequest.FxContributionRequest(pair!, 1, 1, timestamp);
+        var request2 = MarketDataContributionRequest.FxContributionRequest(pair!, 1, 1, timestamp);
+
+        var validationResult1 = MarketDataValidation.MarketDataValidation.Success(MarketDataValidationId.NewId());
+        var validationResult2 = MarketDataValidation.MarketDataValidation.Success(MarketDataValidationId.NewId());
+
+        var repository = GetRepository();
+        Assert.DoesNotThrowAsync(() => repository.Store(request1, validationResult1));
+        Assert.ThrowsAsync<QuoteAlreadyExistException>(() => repository.Store(request2, validationResult2));
+    }
+}
